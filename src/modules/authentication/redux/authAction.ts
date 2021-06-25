20baf6b0f83e4d0259b060518abd80e35f55f351
Mislav Petrina <petrinamislav@gmail.com>
2021-06-25 15:46:26 +0200

    fixed configureStore error, fixed authAction

--- src/modules/authentication/redux/authAction.ts
+++ src/modules/authentication/redux/authAction.ts
@@ -2,13 +2,7 @@ import { createAction } from 'modules/redux-store';
 
 import firebase from 'firebase/app';
 
-interface State {
-  user: firebase.User | null;
-  loading: boolean;
-  error?: string;
-}
-
 export const AuthAction = {
-  add: (item: State) => createAction('user/add', item),
+  add: (item: firebase.User) => createAction('user/add', item),
   error: (item: string) => createAction('user/error', item),
 };
