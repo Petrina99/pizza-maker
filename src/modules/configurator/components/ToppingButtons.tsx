052a5c9d71aff14249a2adb558f60b79957f93ac
Mislav Petrina <petrinamislav@gmail.com>
2021-07-06 20:01:51 +0200

    changed mapping for buttons

--- src/modules/configurator/components/ToppingButtons.tsx
+++ src/modules/configurator/components/ToppingButtons.tsx
@@ -30,76 +30,19 @@ export const ToppingButtons: React.FC = () => {
 
   return (
     <div>
-      <button
-        data-id={toppingArray[0].id}
-        value={toppingArray[0].title}
-        onClick={handleClick}
-        name='chilli'
-        type='button'
-      >
-        <img src={toppingArray[0].image} />
-        {toppingArray[0].title}
-      </button>
-      <button
-        data-id={toppingArray[1].id}
-        value={toppingArray[1].title}
-        onClick={handleClick}
-        name='corn'
-        type='button'
-      >
-        <img src={toppingArray[1].image} />
-        {toppingArray[1].title}
-      </button>
-      <button
-        data-id={toppingArray[2].id}
-        value={toppingArray[2].title}
-        onClick={handleClick}
-        name='egg'
-        type='button'
-      >
-        <img src={toppingArray[2].image} />
-        {toppingArray[2].title}
-      </button>
-      <button
-        data-id={toppingArray[3].id}
-        value={toppingArray[3].title}
-        onClick={handleClick}
-        name='pineapple'
-        type='button'
-      >
-        <img src={toppingArray[4].image} />
-        {toppingArray[3].title}
-      </button>
-      <button
-        data-id={toppingArray[4].id}
-        value={toppingArray[4].title}
-        onClick={handleClick}
-        name='meat'
-        type='button'
-      >
-        <img src={toppingArray[5].image} />
-        {toppingArray[4].title}
-      </button>
-      <button
-        data-id={toppingArray[5].id}
-        value={toppingArray[5].title}
-        onClick={handleClick}
-        name='shrooms'
-        type='button'
-      >
-        <img src={toppingArray[6].image} />
-        {toppingArray[5].title}
-      </button>
-      <button
-        data-id={toppingArray[6].id}
-        value={toppingArray[6].title}
-        onClick={handleClick}
-        name='bacon'
-        type='button'
-      >
-        <img src={toppingArray[6].image} />
-        {toppingArray[6].title}
-      </button>
+      {toppingArray.map((topping) => (
+        <button
+          data-id={topping.id}
+          key={topping.id}
+          value={topping.title}
+          onClick={handleClick}
+          name={topping.title}
+          type='button'
+        >
+          <img src={topping.image} />
+          {topping.title}
+        </button>
+      ))}
     </div>
   );
 };
