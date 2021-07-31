7e97d375ab1895de6b32d9bd07f46463a8e95493
Mislav Petrina <petrinamislav@gmail.com>
2021-07-31 19:33:03 +0200

    finished all css

--- src/modules/success/SuccessPage.tsx
+++ src/modules/success/SuccessPage.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
-import pizza from '../../images/pizza-success.svg';
+import pizza from 'images/pizza-success.svg';
+
+import style from './styles/success.module.css';
+
 export const SuccessPage: React.FC = () => {
   return (
-    <div className='order-success'>
-      <div className='pizza-success'>
-        <img src={pizza} alt='Pizza success' />
+    <div className={style.successDiv}>
+      <div className={style.imgDiv}>
+        <img src={pizza} alt='Pizza success' className={style.img} />
       </div>
-      <div className='success-div'>
-        <p className='success-title'>Your Pizza! Pizza!</p>
-        <p className='success-title2'>is on its way!</p>
-        <p className='success-par'>You should be enjoying your meal in no</p>
-        <p className='success-par2'>more than 45 minutes.</p>
+      <div className={style.textDiv}>
+        <div className={style.boldDiv}>
+          <p className={style.title}>Your Pizza! Pizza!</p>
+          <p className={style.title}>is on its way!</p>
+        </div>
+        <div className={style.smallTextDiv}>
+          <p className={style.p}>You should be enjoying your meal in no</p>
+          <p className={style.p}>more than 45 minutes.</p>
+        </div>
       </div>
     </div>
   );
