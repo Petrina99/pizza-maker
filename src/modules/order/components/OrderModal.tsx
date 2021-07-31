40abf409f57a228ac6f8f128b98c305a0cea2fa0
Mislav Petrina <petrinamislav@gmail.com>
2021-07-31 17:51:25 +0200

    orderModal css

--- src/modules/order/components/OrderModal.tsx
+++ src/modules/order/components/OrderModal.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ShippingInfo, OrderDetails } from './index';
 
+import style from '../styles/orderModal.module.css';
+
 export const OrderModal: React.FC = () => {
   return (
-    <div className='order-modal'>
+    <div className={style.orderModal}>
       <OrderDetails />
       <ShippingInfo />
     </div>
