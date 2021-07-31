2516e2f92387816e196da110f580dd0d807a142d
Mislav Petrina <petrinamislav@gmail.com>
2021-07-31 19:12:34 +0200

    finished order components

--- src/modules/order/components/ShippingInfo.tsx
+++ src/modules/order/components/ShippingInfo.tsx
@@ -16,6 +16,8 @@ import { useHistory } from 'react-router';
 
 import { useOrder } from 'modules/order/hooks';
 
+import style from '../styles/shippingInfo.module.css';
+
 type FormValues = {
   address: string;
   city: string;
@@ -61,54 +63,69 @@ export const ShippingInfo: React.FC = () => {
   };
 
   return (
-    <div>
-      <form
-        className='shipping-form'
-        onSubmit={handleSubmit(onSubmit, onError)}
-      >
-        <p>Shipping information</p>
+    <div className={style.shippingInfo}>
+      <form className={style.form} onSubmit={handleSubmit(onSubmit, onError)}>
+        <p className={style.shippingTitle}>Shipping information</p>
         <input
           {...register('address', {
             required: 'Street name and number field is required.',
           })}
           id='address'
           placeholder='Street name and number'
+          className={style.streetInput}
         />
-        {errors.address && <p>{errors.address.message}</p>}
-        <input
-          {...register('city', {
-            required: 'City field is required.',
-          })}
-          id='city'
-          placeholder='City'
-        />
-        {errors.city && <p>{errors.city.message}</p>}
-        <input
-          type='number'
-          {...register('postalCode', {
-            required: 'Postal code field is required.',
-            minLength: {
-              value: 5,
-              message: 'Please enter a valid postal code.',
-            },
-            maxLength: {
-              value: 5,
-              message: 'Please enter a valid postal code.',
-            },
-          })}
-          id='postal-code'
-          placeholder='Postal Code'
-        />
-        {errors.postalCode && <p>{errors.postalCode.message}</p>}
+        {errors.address && (
+          <p className={style.errorMessage}>{errors.address.message}</p>
+        )}
+        <div className={style.cityPostalDiv}>
+          <input
+            {...register('city', {
+              required: 'City field is required.',
+            })}
+            id='city'
+            placeholder='City'
+            className={style.cityPostalInput}
+          />
+          <input
+            type='number'
+            {...register('postalCode', {
+              required: 'Postal code field is required.',
+              minLength: {
+                value: 5,
+                message: 'Please enter a valid postal code.',
+              },
+              maxLength: {
+                value: 5,
+                message: 'Please enter a valid postal code.',
+              },
+            })}
+            id='postal-code'
+            placeholder='Postal Code'
+            className={style.cityPostalInput}
+          />
+        </div>
+        {errors.city && (
+          <p className={style.errorMessage}>{errors.city.message}</p>
+        )}
+        {errors.postalCode && (
+          <p className={style.errorMessage}>{errors.postalCode.message}</p>
+        )}
         <input
           {...register('country', { required: 'Country filed is required.' })}
           id='country'
           placeholder='Country'
+          className={style.countryInput}
         />
-        {errors.country && <p>{errors.country.message}</p>}
-        <p>Payment details</p>
-        <p>Cash on delivery</p>
-        <button type='submit'>Finish order</button>
+        {errors.country && (
+          <p className={style.errorMessage}>{errors.country.message}</p>
+        )}
+        <p className={style.paymentDetails}>Payment details</p>
+        <p className={style.paymentMethod}>Cash on delivery</p>
+        <div className={style.submitDiv}>
+          <button type='submit' className={style.finishBtn}>
+            Finish order
+          </button>
+        </div>
       </form>
     </div>
   );
